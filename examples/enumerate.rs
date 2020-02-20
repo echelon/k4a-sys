b40830d1d64a762e1993aa560e6eaeb587c7609f
Amédée d'Aboville <amedee.daboville@gmail.com>
2020-02-20 16:04:47 -0500

    Switch to rustified enums. I didn't know about this setting when I created this crate, and went and duplicated it in the -rs crate, so it's quite handy. (Probably will still go rename and alias things to be shorter in the -rs crate anyway). Also it sidesteps a portability issue where linux and windows enums were bindgen-ing different #repr()s (linux #[repr(i32)] and windows #repr(u32) iirc), which is apparently a bug in msvc.

--- examples/enumerate.rs
+++ examples/enumerate.rs
@@ -9,14 +9,14 @@ fn main() {
         let device_count = k4a_device_get_installed_count();
         println!("Found {} connected devices:", device_count);
         for device_idx in 0..device_count {
-            if k4a_device_open(device_idx, &mut device) != k4a_buffer_result_t_K4A_BUFFER_RESULT_SUCCEEDED {
+            if k4a_device_open(device_idx, &mut device) != k4a_result_t::K4A_RESULT_SUCCEEDED {
                 println!("Failed to open device #{}", device_idx);
                 continue;
             }
             let mut serial_number_length: usize = 0;
 
             if k4a_device_get_serialnum(device, ptr::null_mut(), &mut serial_number_length) != 
-                k4a_buffer_result_t_K4A_BUFFER_RESULT_TOO_SMALL {
+                k4a_buffer_result_t::K4A_BUFFER_RESULT_TOO_SMALL {
                 println!("{}: Failed to get serial number length", device_idx);
                 k4a_device_close(device);
                 device = ptr::null_mut();
@@ -27,7 +27,7 @@ fn main() {
             let serial_number_ptr = (&mut serial_number).as_mut_ptr();
 
             if k4a_device_get_serialnum(device, serial_number_ptr, &mut serial_number_length) != 
-                k4a_buffer_result_t_K4A_BUFFER_RESULT_SUCCEEDED {
+                k4a_buffer_result_t::K4A_BUFFER_RESULT_SUCCEEDED {
                 println!("{}: Failed to get serial number", device_idx);
                 k4a_device_close(device);
                 device = ptr::null_mut();
--- examples/streaming.rs
+++ examples/streaming.rs
@@ -7,13 +7,12 @@ use k4a_sys::*;
 fn open_device() -> Option<k4a_device_t> {
     unsafe {
         let device_count = k4a_sys::k4a_device_get_installed_count();
-        let mut device: k4a_device_t = ptr::null_mut();
         if device_count == 0 {
             println!("No K4A devices found");
             return None;
         }
-
-        if k4a_device_open(0, &mut device) != k4a_buffer_result_t_K4A_BUFFER_RESULT_SUCCEEDED {
+        let mut device: k4a_device_t = ptr::null_mut();
+        if k4a_device_open(0, &mut device) != k4a_result_t::K4A_RESULT_SUCCEEDED {
             println!("Failed to open device");
             return None;
         }
@@ -21,26 +20,7 @@ fn open_device() -> Option<k4a_device_t> {
     }
 }
 
-pub struct DeviceConfiguration (k4a_device_configuration_t);
-impl DeviceConfiguration {
-    pub fn new() -> Self {
-     Self (k4a_sys::k4a_device_configuration_t {
-        color_format: k4a_image_format_t_K4A_IMAGE_FORMAT_COLOR_MJPG,
-        color_resolution: k4a_color_resolution_t_K4A_COLOR_RESOLUTION_720P,
-        depth_mode: k4a_depth_mode_t_K4A_DEPTH_MODE_WFOV_2X2BINNED,
-        camera_fps: 0,
-        synchronized_images_only: false,
-        depth_delay_off_color_usec: 0,
-        wired_sync_mode: 0,
-        subordinate_delay_off_master_usec: 0,
-        disable_streaming_indicator: false,
-        }
-     )
-    }
-}
-
 fn main() {
-    unsafe {
     let timeout_in_ms = 1000;
 
     let args = env::args().collect::<Vec<String>>();
@@ -53,29 +33,35 @@ fn main() {
     let capture_frame_count: i32 = args[1].parse().expect("Unable to parse frame count as a number");
     println!("Capturing {} frames", capture_frame_count);
 
+    let config = k4a_device_configuration_t {
+        color_format : k4a_image_format_t::K4A_IMAGE_FORMAT_COLOR_MJPG,
+        color_resolution : k4a_color_resolution_t::K4A_COLOR_RESOLUTION_2160P,
+        depth_mode : k4a_depth_mode_t::K4A_DEPTH_MODE_NFOV_UNBINNED,
+        camera_fps : k4a_fps_t::K4A_FRAMES_PER_SECOND_30,
+        synchronized_images_only: false,
+        depth_delay_off_color_usec: 0,
+        wired_sync_mode: k4a_wired_sync_mode_t::K4A_WIRED_SYNC_MODE_STANDALONE,
+        subordinate_delay_off_master_usec: 0,
+        disable_streaming_indicator: false,
+    };
+    unsafe {
     let device: k4a_device_t = open_device().expect("Unable to open device");
-    let mut config = DeviceConfiguration::new();
-    config.0.color_format = k4a_image_format_t_K4A_IMAGE_FORMAT_COLOR_MJPG;
-    config.0.color_resolution = k4a_color_resolution_t_K4A_COLOR_RESOLUTION_2160P;
-    config.0.depth_mode = k4a_depth_mode_t_K4A_DEPTH_MODE_NFOV_UNBINNED;
-    config.0.camera_fps = k4a_fps_t_K4A_FRAMES_PER_SECOND_30;
 
-    if k4a_device_start_cameras(device, &(config.0)) != k4a_buffer_result_t_K4A_BUFFER_RESULT_SUCCEEDED {
+    if k4a_device_start_cameras(device, &config) != k4a_result_t::K4A_RESULT_SUCCEEDED {
         panic!("Failed to start device");
     }
 
     let mut capture: k4a_capture_t = ptr::null_mut();
     for _ in 0..capture_frame_count {
         match k4a_device_get_capture(device, &mut capture, timeout_in_ms) {
-            k4a_wait_result_t_K4A_WAIT_RESULT_SUCCEEDED => {}
-            k4a_wait_result_t_K4A_WAIT_RESULT_TIMEOUT => {
+            k4a_wait_result_t::K4A_WAIT_RESULT_SUCCEEDED => {}
+            k4a_wait_result_t::K4A_WAIT_RESULT_TIMEOUT => {
                 println!("Timed out waiting for a capture");
                 continue;
             }
-            k4a_wait_result_t_K4A_WAIT_RESULT_FAILED => {
+            k4a_wait_result_t::K4A_WAIT_RESULT_FAILED => {
                 panic!("Failed to read a capture");
             }
-            _ => panic!("Unknown result from device capture")
         }
         println!("Capture");
 
